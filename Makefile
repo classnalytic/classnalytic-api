80ab7274aff5a206f3d0c5771730901e3af3f0cc
Wiput Pootong <wiput.pootong@gmail.com>
2019-01-03 17:36:48 +0700

    :tada: initial commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+build: 
+	docker build -t registry.itforge.io/classnalytic/api .
+push:
+	docker push registry.itforge.io/classnalytic/api
---
+++ models/Rooms.js
@@ -0,0 +1,13 @@
+const uuid = require('uuid/v4')
+
+module.exports = (sequelize, type) => {
+  return sequelize.define('rooms', {
+    id: {
+      type: type.UUID,
+      defaultValue: uuid(),
+      primaryKey: true
+    },
+    name: { type: type.STRING, notEmpty: false },
+    description: type.TEXT
+  })
+}
